27ddac0ef9f113a113b12610036b880b6e5312d0
struckm94 <matthewstruck@gmail.com>
2022-01-28 13:46:17 -0500

    Win Conditions updated

--- TicTacToe.py
+++ TicTacToe.py
@@ -17,39 +17,39 @@ def winner_Calc():
     
     #Player 1 checks
     
-    #Horizontal Checks
-    if 'x' == (row1[0] and row1[1] and row1[2]):
+    #Horizontal Checks for Player 1
+    if 'x' == row1[0] and 'x' == row1[1] and 'x' == row1[2]:
         print(f"{player1} wins!")
         winner = True
         return(winner)
-    elif 'x' == (row2[0] and row2[1] and row2[2]):
+    elif 'x' == row2[0] and 'x' == row2[1] and 'x' == row2[2]:
         print(f"{player1} wins!")
         winner = True
         return(winner)        
-    elif 'x' == (row3[0] and row3[1] and row3[2]):
+    elif 'x' == row3[0] and 'x' == row3[1] and 'x' == row3[2]:
         print(f"{player1} wins!")
         winner = True
         return(winner)
-    #Diagonal Checks
-    elif 'x' == (row1[0] and row2[1] and row3[2]):
+    #Diagonal Checks for Player 1
+    elif 'x' == row1[0] and 'x' == row2[1] and 'x' == row3[2]:
         print(f"{player1} wins!")
         winner = True
         return(winner)
-    elif 'x' == (row1[2] and row2[1] and row3[0]):
+    elif 'x' == row1[2] and 'x' == row2[1] and 'x' == row3[0]:
         print(f"{player1} wins!")
         winner = True
         return(winner)
 
-    #Vertical checks
-    elif 'x' == (row1[0] and row2[0] and row3[0]):
+    #Vertical checks for Player 1
+    elif 'x' == row1[0] and 'x' == row2[0] and 'x' == row3[0]:
         print(f"{player1} wins!")
         winner = True
         return(winner)        
-    elif 'x' == (row1[1] and row2[1] and row3[1]):
+    elif 'x' == row1[1] and 'x' == row2[1] and 'x' == row3[1]:
         print(f"{player1} wins!")
         winner = True
         return(winner)
-    elif 'x' == (row1[2] and row2[2] and row3[2]):
+    elif 'x' == row1[2] and 'x' == row2[2] and 'x' == row3[2]:
         print(f"{player1} wins!")
         winner = True
         return(winner)    
@@ -57,42 +57,43 @@ def winner_Calc():
     
     #Player 2 checks
     
-    #Horizontal Checks
-    if 'o' == (row1[0] and row1[1] and row1[2]):
+    #Horizontal Checks for Player 2
+    if 'o' == row1[0] and 'o' == row1[1] and 'o' == row1[2]:
         print(f"{player2} wins!")
         winner = True
         return(winner)
-    elif 'o' == (row2[0] and row2[1] and row2[2]):
+    elif 'o' == row2[0] and 'o' == row2[1] and 'o' == row2[2]:
         print(f"{player2} wins!")
         winner = True
         return(winner)        
-    elif 'o' == (row3[0] and row3[1] and row3[2]):
+    elif 'o' == row3[0] and 'o' == row3[1] and 'o' == row3[2]:
         print(f"{player2} wins!")
         winner = True
         return(winner)
-    #Diagonal Checks
-    elif 'o' == (row1[0] and row2[1] and row3[2]):
+    #Diagonal Checks for Player 2
+    elif 'o' == row1[0] and 'o' == row2[1] and 'o' == row3[2]:
         print(f"{player2} wins!")
         winner = True
         return(winner)
-    elif 'o' == (row1[2] and row2[1] and row3[0]):
+    elif 'o' == row1[2] and 'o' == row2[1] and 'o' == row3[0]:
         print(f"{player2} wins!")
         winner = True
         return(winner)
 
-    #Vertical checks
-    elif 'o' == (row1[0] and row2[0] and row3[0]):
+    #Vertical checks for Player 2
+    elif 'o' == row1[0] and 'o' == row2[0] and 'o' == row3[0]:
         print(f"{player2} wins!")
         winner = True
         return(winner)        
-    elif 'o' == (row1[1] and row2[1] and row3[1]):
+    elif 'o' == row1[1] and 'o' == row2[1] and 'o' == row3[1]:
         print(f"{player2} wins!")
         winner = True
         return(winner)
-    elif 'o' == (row1[2] and row2[2] and row3[2]):
+    elif 'o' == row1[2] and 'o' == row2[2] and 'o' == row3[2]:
         print(f"{player2} wins!")
         winner = True
         return(winner)  
+
 
     
 
@@ -117,6 +118,10 @@ while True:
 
     #Calculate Winner!
     if winner_Calc() == True:
+        break
+    
+    elif turnCount == 9 and winner != True:
+        print("Cat wins!")
         break
 
     #Determine player's turn
